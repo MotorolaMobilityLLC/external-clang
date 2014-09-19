cae83fcbb059282a13d2a78fc78d4e314d3d5747
Tong Shen <endlessroad@google.com>
2014-09-19 12:02:00 -0700

    Add libclang for target.
    Change-Id: I3dc5de6c1a4e7f5ba64196dd8ee0965ef10d4e8b

--- lib/Lex/Android.mk
+++ lib/Lex/Android.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-# For the host only
-# =====================================================
-include $(CLEAR_VARS)
 include $(CLEAR_TBLGEN_VARS)
 
 TBLGEN_TABLES := \
@@ -33,13 +30,26 @@ clang_lex_SRC_FILES := \
   TokenConcatenation.cpp \
   TokenLexer.cpp
 
+# For the host
+# =====================================================
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := $(clang_lex_SRC_FILES)
-
 LOCAL_MODULE:= libclangLex
-LOCAL_MODULE_TAGS := optional
-
 LOCAL_MODULE_TAGS := optional
 
 include $(CLANG_HOST_BUILD_MK)
 include $(CLANG_TBLGEN_RULES_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the target
+# =====================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(clang_lex_SRC_FILES)
+LOCAL_MODULE:= libclangLex
+LOCAL_MODULE_TAGS := optional
+
+include $(CLANG_DEVICE_BUILD_MK)
+include $(CLANG_TBLGEN_RULES_MK)
+include $(BUILD_STATIC_LIBRARY)
